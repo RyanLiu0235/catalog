d30404fc23c3cddc767938a2bad2868e51210bf7
刘成 <emitremmus0235@163.com>
2016-09-19 19:42:55 +0800

    增加chalk

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var fs = require('fs');
+var chalk = require('chalk');
+var log = console.log;
 
 fs.readFile('./package.json', function(err, data) {
 	if (err) {
 		throw err;
 	}
 	var _settings = JSON.parse(data.toString()).scripts;
+	log(chalk.yellow('scripts available:'));
 	for (var i in _settings) {
-		console.log(i + ': ' + _settings[i]);
-		console.log('\n');
+		log('    ' + chalk.blue(i + ': ') + chalk.green(_settings[i]));
 	}
+	log('\n');
 });
